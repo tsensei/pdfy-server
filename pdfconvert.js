d6c6eb6d58460feff45bade0935a3ecd0ab31d1f
tsensei <tjsiam019@gmail.com>
2021-06-11 18:27:32 +0600

    server upload 2

--- pdfconvert.js
+++ pdfconvert.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 
 const createPdf = async (url, uuid) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
   const page = await browser.newPage();
   const options = {
     path: `pdf/${uuid}.pdf`,
